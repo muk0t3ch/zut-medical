40bfcffff0b5060fa7cd65dfef5a904383ed82ee
muk0t3ch <77383591+muk0t3ch@users.noreply.github.com>
2023-04-24 09:24:50 +0200

    intial commit

--- src/app/components/Brand.jsx
+++ src/app/components/Brand.jsx
@@ -4,37 +4,44 @@ import useSettings from 'app/hooks/useSettings';
 import { Span } from './Typography';
 
 const BrandRoot = styled(Box)(() => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: '20px 18px 20px 29px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '20px 18px 20px 29px',
 }));
 
 const StyledSpan = styled(Span)(({ mode }) => ({
-  fontSize: 18,
-  marginLeft: '.5rem',
-  display: mode === 'compact' ? 'none' : 'block',
+    fontSize: 18,
+    marginLeft: '.5rem',
+    display: mode === 'compact' ? 'none' : 'block',
 }));
 
 const Brand = ({ children }) => {
-  const { settings } = useSettings();
-  const leftSidebar = settings.layout1Settings.leftSidebar;
-  const { mode } = leftSidebar;
+    const { settings } = useSettings();
+    const leftSidebar = settings.layout1Settings.leftSidebar;
+    const { mode } = leftSidebar;
 
-  return (
-    <BrandRoot>
-      <Box display="flex" alignItems="center">
-        <MatxLogo />
-        <StyledSpan mode={mode} className="sidenavHoverShow">
-          ZUCT
-        </StyledSpan>
-      </Box>
+    return ( <
+        BrandRoot >
+        <
+        Box display = "flex"
+        alignItems = "center" >
+        <
+        MatxLogo / >
+        <
+        StyledSpan mode = { mode }
+        className = "sidenavHoverShow" >
+        ZUCT MEDICAL <
+        /StyledSpan> <
+        /Box>
 
-      <Box className="sidenavHoverShow" sx={{ display: mode === 'compact' ? 'none' : 'block' }}>
-        {children || null}
-      </Box>
-    </BrandRoot>
-  );
+        <
+        Box className = "sidenavHoverShow"
+        sx = {
+            { display: mode === 'compact' ? 'none' : 'block' } } > { children || null } <
+        /Box> <
+        /BrandRoot>
+    );
 };
 
 export default Brand;
